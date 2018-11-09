312a71a7105c27115f126ccf49fb440cb43593c5
Niles Christensen <nnc2117@columbia.edu>
2018-11-08 21:45:32 -0500

    [Adds] a blacklist of files to not make it past the filter

--- scraper/filter_debates.py
+++ scraper/filter_debates.py
@@ -6,10 +6,17 @@ import progressbar
 discussion_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'discussions')
 dst_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'english_discussions')
 
+blacklist = set([
+        'compulsory-voting-should-voting-be-mandatory-1692.txt',
+    ])
+
 p = progressbar.ProgressBar(term_width=80)
 print('Filtering and copying English files: ')
 
 for filename in p(os.listdir(discussion_dir)):
+    if filename in blacklist:
+        continue
+
     src_file = os.path.join(discussion_dir, filename)
     dst_file = os.path.join(dst_dir, filename)
     with open(src_file, 'r', encoding='latin-1') as current_file:
