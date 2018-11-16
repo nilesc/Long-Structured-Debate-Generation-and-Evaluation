0537d27d092c3147f70183285be470ab8264e80d
Niles Christensen <nnc2117@columbia.edu>
2018-11-16 20:49:00 +0000

    [Undoes] the last commit

--- setup-cuda.sh
+++ setup-cuda.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-echo "Checking for CUDA and installing."
-# Check for CUDA and try to install.
-if ! dpkg-query -W cuda-9-0; then
-# The 16.04 installer works with 16.10.
-    curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
-    dpkg -i ./cuda-repo-ubuntu1604_9.0.176-1amd64.deb
-    apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repose/ubuntu1604/x86_64/7fa2af80.pub
-    # If the above line throws an error, use the following instead
-    # curl https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub > 7fa2af80.pub
-    # sudo apt-key add *.pub
-    apt-get update
-    apt-get install cuda-9-0 -y
-fi
-nvidia-smi -pm 1
+    echo "Checking for CUDA and installing."
+    if ! dpkg-query -W cuda-9-0; then
+        curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
+        dpkg -i ./cuda-repo-ubuntu1604_9.0.176-1amd64.deb
+        apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repose/ubuntu1604/x86_64/7fa2af80.pub
+        apt-get update
+        apt-get install cuda-9-0 -y
+    fi
+    nvidia-smi -pm 1
--- setup-instance.sh
+++ setup-instance.sh
@@ -12,7 +12,22 @@ yes | sudo apt-get install git
 yes | sudo apt-get install bzip2
 
 # Get CUDA
-bash setup-cuda.sh
+#!/bin/bash
+echo "Checking for CUDA and installing."
+# Check for CUDA and try to install.
+if ! dpkg-query -W cuda-9-0; then
+  # The 16.04 installer works with 16.10.
+  curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
+  dpkg -i ./cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
+  apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
+  # If the above line throws an error, use the following instead
+  # curl https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub > 7fa2af80.pub
+  # sudo apt-key add *.pub
+  apt-get update
+  apt-get install cuda-9-0 -y
+fi
+# Enable persistence mode
+nvidia-smi -pm 1
 
 # Add CUDA to environment (should be added to in ~/.bashrc)
 export PATH=/usr/local/cuda-9.0/bin${PATH:+:${PATH}}
